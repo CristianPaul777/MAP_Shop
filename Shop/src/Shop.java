c8d331dff5b3b24a67f0b29291d108d8ad2e6dcc
Paul Cristian <paolocristiano24@icloud.com>
2025-10-20 02:05:30 +0300

    2

--- Shop/src/Shop.java
+++ Shop/src/Shop.java
@@ -12,5 +12,18 @@ public class Shop {
         for (int x : u) if (x > max) max = x;
         return max;
     }
+    public int maxUsbImBudget(int[] u, int b) {
+        int max = -1;
+        for (int x : u) if (x <= b && x > max) max = x;
+        return max;
+    }
 
+    public int maxAusgabe(int b, int[] t, int[] u) {
+        int maxSum = -1;
+        for (int x : t)
+            for (int y : u)
+                if (x + y <= b && x + y > maxSum)
+                    maxSum = x + y;
+        return maxSum;
+    }
 }
